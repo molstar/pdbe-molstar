c105974035fb35e69d9a5cc8da9169aaf9356a21
Adam Midlik <midlik@gmail.com>
2024-01-15 11:18:55 +0000

    Simplify and lint config

--- gulpfile.js
+++ gulpfile.js
@@ -6,48 +6,72 @@ var concat = require('gulp-concat');
 var header = require('gulp-header');
 
 const PACKAGE_ROOT_PATH = process.cwd();
-const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, "package.json"));
+const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, 'package.json'));
 
-const banner = ['/**',
-  ` * ${PKG_JSON.name}`,
-  ` * @version ${PKG_JSON.version}`,
-  ' * @link https://github.com/PDBeurope/pdbe-molstar',
-  ' * @license Apache 2.0',
-  ' */',
-  ''].join('\n');
+const banner = [
+    '/**',
+    ` * ${PKG_JSON.name}`,
+    ` * @version ${PKG_JSON.version}`,
+    ' * @link https://github.com/PDBeurope/pdbe-molstar',
+    ' * @license Apache 2.0',
+    ' */',
+    '',
+].join('\n');
 
-  const license = ['/**',
-  ' * Copyright 2019-2020 Mandar Deshpande <mandar@ebi.ac.uk>',
-  ' * European Bioinformatics Institute (EBI, http://www.ebi.ac.uk/)',
-  ' * European Molecular Biology Laboratory (EMBL, http://www.embl.de/)',
-  ' * Licensed under the Apache License, Version 2.0 (the "License");',
-  ' * you may not use this file except in compliance with the License.',
-  ' * You may obtain a copy of the License at ',
-  ' * http://www.apache.org/licenses/LICENSE-2.0',
-  ' * ',
-  ' * Unless required by applicable law or agreed to in writing, software',
-  ' * distributed under the License is distributed on an "AS IS" BASIS, ',
-  ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
-  ' * See the License for the specific language governing permissions and ',
-  ' * limitations under the License.',
-  ' */',
-  ''].join('\n');
+const license = [
+    '/**',
+    ' * Copyright 2019-2020 Mandar Deshpande <mandar@ebi.ac.uk>',
+    ' * European Bioinformatics Institute (EBI, http://www.ebi.ac.uk/)',
+    ' * European Molecular Biology Laboratory (EMBL, http://www.embl.de/)',
+    ' * Licensed under the Apache License, Version 2.0 (the "License");',
+    ' * you may not use this file except in compliance with the License.',
+    ' * You may obtain a copy of the License at ',
+    ' * http://www.apache.org/licenses/LICENSE-2.0',
+    ' * ',
+    ' * Unless required by applicable law or agreed to in writing, software',
+    ' * distributed under the License is distributed on an "AS IS" BASIS, ',
+    ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
+    ' * See the License for the specific language governing permissions and ',
+    ' * limitations under the License.',
+    ' */',
+    '',
+].join('\n');
 
-
-gulp.task('clean-all', function() {
+gulp.task('clean-all', function () {
     return del(['lib', 'build', 'tsconfig.tsbuildinfo']);
 });
 
-gulp.task('clean-component', function() {
+gulp.task('clean-component', function () {
     return del([`build/${PKG_JSON.name}-component-${PKG_JSON.version}.js`]);
 });
 
 gulp.task('concat', function () {
-    return gulp.src([`build/${PKG_JSON.name}-plugin-${PKG_JSON.version}.js`,`lib/${PKG_JSON.name}-component-build-${PKG_JSON.version}.js`])
+    return gulp
+        .src([
+            `build/${PKG_JSON.name}-plugin-${PKG_JSON.version}.js`,
+            `lib/${PKG_JSON.name}-component-build-${PKG_JSON.version}.js`,
+        ])
         .pipe(concat(`${PKG_JSON.name}-component-${PKG_JSON.version}.js`))
-        .pipe(header(license, {} ))
-		.pipe(header(banner, {} ))
+        .pipe(header(license, {}))
+        .pipe(header(banner, {}))
         .pipe(gulp.dest('build/'));
 });
 
 gulp.task('bundle-webcomponent', gulp.series('clean-component', 'concat'));
+
+function replaceSkin(srcFile, skin) {
+    const destFile = path.join(
+        path.dirname(srcFile),
+        `_${skin}_${path.basename(srcFile)}`
+    );
+    let text = fs.readFileSync(srcFile, { encoding: 'utf8' });
+    text = text.replace(
+        'mol-plugin-ui/skin/dark.scss',
+        `mol-plugin-ui/skin/${skin}.scss`
+    );
+    fs.writeFileSync(destFile, text, { encoding: 'utf8' });
+}
+gulp.task('skin', async function () {
+    replaceSkin('lib/index.js', 'light');
+    replaceSkin('lib/index.d.ts', 'light');
+});
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -4,123 +4,126 @@ const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PACKAGE_ROOT_PATH = process.cwd();
-const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, "package.json"));
+const PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, 'package.json'));
 
 const molstarConfig = {
     target: 'web',
     entry: path.resolve(__dirname, `lib/index.js`),
-    output: { filename: `${PKG_JSON.name}-plugin-${PKG_JSON.version}.js`, path: path.resolve(__dirname, `build/`) },
+    output: {
+        filename: `${PKG_JSON.name}-plugin-${PKG_JSON.version}.js`,
+        path: path.resolve(__dirname, `build/`),
+    },
     module: {
         rules: [
             {
                 test: /\.(html|ico)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: { name: '[name].[ext]' }
-                }]
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { name: '[name].[ext]' },
+                    },
+                ],
             },
             {
                 test: /\.(s*)css$/,
                 use: [
-                  MiniCssExtractPlugin.loader,
-                  { loader: 'css-loader', options: { sourceMap: false } },
-                  { loader: 'sass-loader', options: { sourceMap: false } },
-                ]
-            }
-        ]
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: false } },
+                    { loader: 'sass-loader', options: { sourceMap: false } },
+                ],
+            },
+        ],
     },
     plugins: [
         new ExtraWatchWebpackPlugin({
-            files: [
-                './lib/**/*.scss',
-                './lib/**/*.html'
-            ],
+            files: ['./lib/**/*.scss', './lib/**/*.html'],
         }),
         new webpack.DefinePlugin({
-          'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
-          '__MOLSTAR_DEBUG_TIMESTAMP__': webpack.DefinePlugin.runtimeValue(() => `${new Date().valueOf()}`, true)
+            'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+            __MOLSTAR_DEBUG_TIMESTAMP__: webpack.DefinePlugin.runtimeValue(
+                () => `${new Date().valueOf()}`,
+                true
+            ),
         }),
-        new MiniCssExtractPlugin({ filename: `${PKG_JSON.name}-${PKG_JSON.version}.css` })
+        new MiniCssExtractPlugin({
+            filename: `${PKG_JSON.name}-${PKG_JSON.version}.css`,
+        }),
     ],
     resolve: {
-      modules: [
-        'node_modules',
-        path.resolve(__dirname, 'lib/')
-      ],
-      fallback: {
-          fs: false,
-          crypto: require.resolve('crypto-browserify'),
-          path: require.resolve('path-browserify'),
-          stream: require.resolve('stream-browserify'),
-      },
-      alias:{
-          Molstar: 'molstar/lib'
-      }
+        modules: ['node_modules', path.resolve(__dirname, 'lib/')],
+        fallback: {
+            fs: false,
+            crypto: require.resolve('crypto-browserify'),
+            path: require.resolve('path-browserify'),
+            stream: require.resolve('stream-browserify'),
+        },
+        alias: {
+            Molstar: 'molstar/lib',
+        },
     },
     watchOptions: {
-        aggregateTimeout: 750
+        aggregateTimeout: 750,
     },
-}
+};
 
 const componentConfig = {
     entry: path.resolve(__dirname, `src/web-component/index.js`),
-    output: { filename: `${PKG_JSON.name}-component-build-${PKG_JSON.version}.js`, path: path.resolve(__dirname, `lib/`) },
-    target: "web",
+    output: {
+        filename: `${PKG_JSON.name}-component-build-${PKG_JSON.version}.js`,
+        path: path.resolve(__dirname, `lib/`),
+    },
+    target: 'web',
     resolve: {
-      extensions: [".js"]
+        extensions: ['.js'],
     },
     externals: {
-      "PDBeMolstarPlugin": "PDBeMolstarPlugin"
+        PDBeMolstarPlugin: 'PDBeMolstarPlugin',
     },
     module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            "style-loader",
-            { loader: "css-loader", options: { importLoaders: 1 } }
-          ]
-        },
-        {
-          test: /.(jpg|jpeg|png|svg)$/,
-          use: ['url-loader'],
-        },
-        {
-          test: /\.(js)$/,
-          exclude: function excludeCondition(path){
-              
-              const nonEs5SyntaxPackages = [
-                'lit-element',
-                'lit-html'
-              ]
-              
-              // DO transpile these packages
-              if (nonEs5SyntaxPackages.some( pkg => path.match(pkg))) {
-                return false;
-              }
-            
-              // Ignore all other modules that are in node_modules
-              if (path.match(/node_modules\\/)) { return true; }
-            
-              else return false;
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                ],
             },
-          use: {
-            loader: "babel-loader",
-            options: {
-              babelrc: false,
-              plugins: [
-                [
-                  "@babel/plugin-transform-runtime",
-                  {
-                    regenerator: true
-                  }
-                ]
-              ]
-            }
-          }
-        }
-      ]
-    }
-}
+            {
+                test: /.(jpg|jpeg|png|svg)$/,
+                use: ['url-loader'],
+            },
+            {
+                test: /\.(js)$/,
+                exclude: function excludeCondition(path) {
+                    const nonEs5SyntaxPackages = ['lit-element', 'lit-html'];
+
+                    // DO transpile these packages
+                    if (nonEs5SyntaxPackages.some((pkg) => path.match(pkg))) {
+                        return false;
+                    }
+
+                    // Ignore all other modules that are in node_modules
+                    if (path.match(/node_modules\\/)) {
+                        return true;
+                    } else return false;
+                },
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: false,
+                        plugins: [
+                            [
+                                '@babel/plugin-transform-runtime',
+                                {
+                                    regenerator: true,
+                                },
+                            ],
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+};
 
 module.exports = [molstarConfig, componentConfig];
