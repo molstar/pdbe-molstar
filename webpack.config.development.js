65a98bf99e92f760f9e09e82bad0cacff636ccc6
Adam Midlik <midlik@gmail.com>
2024-08-21 12:00:18 +0100

    Reorganize CSS files and simplify build scripts

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,21 +1,8 @@
-const path = require('path');
-
-const PACKAGE_ROOT_PATH = process.cwd();
-const PACKAGE = require(path.join(PACKAGE_ROOT_PATH, 'package.json'));
-
-function removeKey(obj, key) {
-    if (typeof obj !== 'object') return obj;
-    const result = { ...obj };
-    delete result[key];
-    return result;
-}
-
 const productionConfig = require('./webpack.config.production.js');
 
 const developmentConfig = productionConfig.map(conf => ({
     ...conf,
     devtool: 'eval-source-map',
-    entry: removeKey(conf.entry, `${PACKAGE.name}-light`),  // skip building pdbe-molstar-light in dev mode
 }));
 
 module.exports = developmentConfig;
