34a749a93c8c95729ce6f9eec4ddd351011f9571
Adam Midlik <midlik@gmail.com>
2025-09-05 12:38:21 +0100

    Rework PDBe link button styling

--- src/app/ui/pdbe-viewport-controls.tsx
+++ src/app/ui/pdbe-viewport-controls.tsx
@@ -1,3 +1,4 @@
+import { Button } from 'molstar/lib/mol-plugin-ui/controls/common';
 import { ViewportControls } from 'molstar/lib/mol-plugin-ui/viewport';
 import { PluginCustomState } from '../plugin-custom-state';
 
@@ -9,16 +10,21 @@ export class PDBeViewportControls extends ViewportControls {
 
         return <>
             {showPDBeLink &&
-                <div className='msp-pdbe-link-box msp-viewport-controls-buttons'>
-                    <div className='msp-pdbe-link-bg msp-semi-transparent-background' />
-                    <a className='msp-pdbe-link' target='_blank' href={`https://pdbe.org/${initParams!.moleculeId}`}>
-                        <img className='msp-pdbe-logo' alt='PDBe logo'
-                            src='https://www.ebi.ac.uk/pdbe/entry/static/images/logos/PDBe/logo_T_64.png' />
-                        {initParams!.moleculeId}
-                    </a>
+                <div className='msp-viewport-controls'>
+                    <div className='msp-viewport-controls-buttons'>
+                        <div className='pdbemolstar-pdbe-link-box'>
+                            <div className='msp-semi-transparent-background' />
+                            <a target='_blank' href={`https://pdbe.org/${initParams!.moleculeId}`} style={{ textDecoration: 'none', color: 'initial' }}>
+                                <Button className='msp-btn-link-toggle-on pdbemolstar-pdbe-link' title='Go to PDBe Pages'>
+                                    <img className='pdbemolstar-pdbe-logo' alt='PDBe logo' src='https://www.ebi.ac.uk/pdbe/entry/static/images/logos/PDBe/logo_T_64.png' />
+                                    {initParams!.moleculeId}
+                                </Button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             }
-            <div style={{ position: 'absolute', top: showPDBeLink ? (32 + 4) : 0, right: 0 }}>
+            <div className={showPDBeLink ? 'pdbemolstar-viewport-controls-shifted' : 'pdbemolstar-viewport-controls-normal'}>
                 {super.render()}
             </div>
         </>;
